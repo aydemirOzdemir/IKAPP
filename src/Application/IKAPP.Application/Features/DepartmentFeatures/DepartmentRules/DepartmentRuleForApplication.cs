d64ef2a492c48ed1a04392250ab6f12b651c576e
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-05-03 17:50:40 +0300

    Rule ve Exception Dosyalaması yapıldı

---
+++ src/Application/IKAPP.Application/Features/DepartmentFeatures/DepartmentRules/DepartmentRuleForApplication.cs
@@ -0,0 +1,12 @@
+﻿using IKAPP.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKAPP.Application.Features.DepartmentFeatures.DepartmentRules;
+
+public class DepartmentRuleForApplication:BaseRuleForApplication
+{
+}
---
+++ src/Application/IKAPP.Application/Features/RoleFeatures/RoleRules/RoleRuleForApplication.cs
@@ -0,0 +1,12 @@
+﻿using IKAPP.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKAPP.Application.Features.RoleFeatures.RoleRules;
+
+public class RoleRuleForApplication:BaseRuleForApplication
+{
+}
