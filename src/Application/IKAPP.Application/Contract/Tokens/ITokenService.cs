7febdcab45984f05b25b31c10fed254260503402
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-05-30 16:27:49 +0300

    Token Service Oluşturuldu

---
+++ src/Application/IKAPP.Application/Contract/Tokens/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using IKAPP.Domain.Entities.AggregateModels.Personals;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKAPP.Application.Contract.Tokens;
+
+public interface ITokenService
+{
+    Task<JwtSecurityToken> CreateToken(Personal user, IList<string> roles);
+    string GenerateRefreshToken();
+    ClaimsPrincipal? GetPrincipalFromExpireToken(string? token);
+}
---
+++ src/Infrastructure/IKAPP.Infrasturucture/Tokens/TokenSetting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKAPP.Infrasturucture.Tokens;
+
+public class TokenSetting
+{
+    public string Audience { get; set; }
+    public string Issuer { get; set; }
+    public string Secret { get; set; }
+    public int TokenValidityInMinutes { get; set; }
+}
